b6bee44ff4b9eb0dd17af786cd0871572c9ea098
shari4ov <kenansharifov400@gmail.com>
2022-03-26 18:37:01 +0400

    Updates

---
+++ precache-manifest.3ebb272b38c03edd3f2b4096f83ab2f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e81caf01de15c9f58491",
+    "url": "/tenzieGame/static/css/main.b7813c35.chunk.css"
+  },
+  {
+    "revision": "e81caf01de15c9f58491",
+    "url": "/tenzieGame/static/js/main.e81caf01.chunk.js"
+  },
+  {
+    "revision": "99d07085df6e750e705a",
+    "url": "/tenzieGame/static/js/1.99d07085.chunk.js"
+  },
+  {
+    "revision": "fc3ed1a7f8560f62c79e",
+    "url": "/tenzieGame/static/js/runtime~main.fc3ed1a7.js"
+  },
+  {
+    "revision": "b8ea53ed2dad50165afb3f23923baa22",
+    "url": "/tenzieGame/index.html"
+  }
+];
